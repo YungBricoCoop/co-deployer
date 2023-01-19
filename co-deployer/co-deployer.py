6119fdff40e77dd05678e14d173e3115c46c69d0
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-01-19 22:57:18 +0100

    feat : load config

--- co-deployer/co-deployer.py
+++ co-deployer/co-deployer.py
@@ -13,3 +13,26 @@ from argparse import ArgumentParser
 import paramiko
 from rich import print
 from rich.progress import track
+
+CONFIG_FILE = "deploy.config.json"
+
+def load_config():
+	"""
+	This function reads the configuration from a file named 'deploy.config.json'
+	:param file_path: the path of the file to read
+	:return: the configuration as a dictionary
+	"""
+	try:
+		with open(CONFIG_FILE, "r") as f:
+			config = json.loads(f.read())
+			print(f"[bold green]Configuration file[/bold green]: {CONFIG_FILE} [bold green]loaded[/bold green]")
+			return config
+	except FileNotFoundError:
+		print(f"[bold red]Configuration not found file[/bold red]: {CONFIG_FILE}")
+		sys.exit(1)
+	except json.decoder.JSONDecodeError:
+		print(f"[bold red]Configuration file[/bold red]: {CONFIG_FILE} [bold red]is not valid JSON[/bold red]")
+		sys.exit(1)
+
+if __name__ == "__main__":
+	load_config()
