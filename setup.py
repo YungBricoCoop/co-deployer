cd661d2fcb20ad7099067ab6d94d9b2d7f13f386
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-01-23 20:39:25 +0100

    feat : added info

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="co-deployer",
-    version="0.1",
+	author="Elwan Mayencourt",
+    version="1",
     packages=find_packages(),
     install_requires=[
         "argparse",
